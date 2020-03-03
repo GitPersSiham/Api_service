0c559dba8e1c40f9acf775ec5d1496bcc07c77fa
GitPersSiham <lahouali.siham@outlook.fr>
2020-03-03 14:08:09 +0100

    ajout de la fake api

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require JSON-SERVER
+const jsonServer = require('json-server');
+
+// Returns an express server
+const server = jsonServer.create();
+
+// Specify file to store routes and documents
+const router = jsonServer.router('db.json');
+
+// Tell the generated express server to use json-server's middlewares
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+
+// Tell express to use the routes from db.json
+server.use(router);
+
+// Retrieve port
+const port = require('./src/constants/api').port;
+
+// Listen to port
+server.listen(port, () => {
+  console.log('JSON Server is running')
+})
---
+++ src/utils/fake-products.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const faker = require('faker');
+
+
+const apiURL = require('../constants/api').baseURL;
+const generateProduct = (i) => {
+
+    faker.seed(i);
+    const id = faker.random.uuid();
+    faker.seed(i);
+    const name = faker.company.companyName();
+    faker.seed(i);
+    const image = "http://placeimg.com/1024/768/arch?a=" + i;
+    faker.seed(i);
+    const adress = faker.address.streetAddress()
+    faker.seed(i);
+    const description = faker.lorem.paragraph();
+    faker.seed(i);
+    const night_price = faker.random.number() /10;
+    
+    return {
+        id,
+        name,
+        image,
+        adress,
+        description,
+        night_price,
+        currency: '€',
+    }
+};
+
+
+let i = 0;
+while (i < 6) {
+    const product = generateProduct(i);
+    console.log("PRODUCT " + product.id + "created", product);
+    axios.post( apiURL + 'products/', {
+        ...product,
+    })
+        .then(res => {
+            console.log("inserted ", res.data);
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
+    i++;
+}
